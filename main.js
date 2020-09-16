93fdea613710f8fe37121e2c623c1e25b90937fa
AGW <agiraldow@gmail.com>
2020-09-16 13:22:11 -0400

    Begin to render data on the page

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 var new_num = 40; // global variable
 jQuery(document).ready(function(){ 
 
-    console.log('hi');
+    $.getJSON( "metadata.json", function( data ) {
+        let words = data.metadata.asr_word;
+        // console.log(words);
+        words.forEach(word =>
+            $(".words").append(word.word + ' '));
+
+        // $.each( data, function( key, val ) {
+        //   items.push( "<li id='" + key + "'>" + val + "</li>" );
+        // });
+       
+        // $( "<ul/>", {
+        //   "class": "my-new-list",
+        //   html: items.join( "" )
+        // }).appendTo( "body" );
+      });
 
 });
