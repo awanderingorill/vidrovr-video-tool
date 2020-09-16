0e596508be045a046a7708cb898e80c4390d215c
AGW <agiraldow@gmail.com>
2020-09-16 13:32:43 -0400

    Bind video timeupdate to DOM

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
-var new_num = 40; // global variable
 jQuery(document).ready(function(){ 
 
     $.getJSON( "metadata.json", function( data ) {
         let words = data.metadata.asr_word;
-        // console.log(words);
         words.forEach(word =>
             $(".words").append(word.word + ' '));
+    });
 
-        // $.each( data, function( key, val ) {
-        //   items.push( "<li id='" + key + "'>" + val + "</li>" );
-        // });
-       
-        // $( "<ul/>", {
-        //   "class": "my-new-list",
-        //   html: items.join( "" )
-        // }).appendTo( "body" );
-      });
+    $("#vidrovr-video").bind(
+        "timeupdate", 
+        function(event){
+          onTrackedVideoFrame(this.currentTime);
+    });
+
+    function onTrackedVideoFrame(currentTime){
+        $("#current").text(currentTime);
+    }
 
 });
